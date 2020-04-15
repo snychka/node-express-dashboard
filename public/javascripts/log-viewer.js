4afc2ba6b564d757c27eef70dc3964efb64c2b72
snychka <stefannychka@gmail.com>
2020-04-15 03:59:32 -0600

    done mod4 task 7 (save for task 1, maybe)

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,20 @@
 const connection = new WebSocket('ws://localhost:3000');
 const logWindow = document.querySelector('#log-window');
 
+//const filePath = document.getElementById("#logFilePath").value;
+const filePath = document.getElementById("logFilePath").value;
+
 // 1st doesn't work, 2nd does (I think 1st may be wrong or different)
 //connection.onopen = () => connection.send("Hello from the client!");
 //connection.onopen = () => {connection.send("Hello from the client!")};
-connection.onopen = function() {connection.send("Hello from the client!");};
+connection.onopen = function() {
+
+  if (filePath) {
+    connection.send(filePath);
+  }
+  connection.send("Hello from the client!");
+
+};
 
 //connection.onmessage= function(event) {logWindow.innerHTML = event.data;}
 connection.onmessage = (event) => {
