76b76e9de3b39b7398f0f2c755b5fe7ed46a2ef0
snychka <stefannychka@gmail.com>
2020-04-21 13:51:58 -0600

    syntax error

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,6 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+  const data = [];
+  files.forEach((file) => {
+    if (isDirectory(currentDir, file)) {
+      data.push({
+        name: file,
+        isDirectory: true,
+        path: path.join(query, file)
+      });
+    } else {
+      data.push({
+        name: file,
+        isDirectory: false,
+        path: path.join(query, file),
+        currentDir
+      });
+    }
+    return data;
+  })
 }
 
 function isDirectory(currentDir, file) {
@@ -11,7 +32,20 @@ function isDirectory(currentDir, file) {
 }
 
 function readDir(currentDir, res, query) {
+  fs.readdir(currentDir, (err, files) => {
+    let directoryContents = [];
+    if (!err) {
+      directoryContents = getDirectoryContents(files, currentDir, query);
+    }
+    res.json(directoryContents);
+  })
 }
 
 exports.get = (req, res) => {
+  let currentDir = dir;
+  const query = req.query.path || "":
+  if (query) {
+    currentDir = path.join(currentDir, query);
+  }
+  readDir(currentDir, res, query);
 };
