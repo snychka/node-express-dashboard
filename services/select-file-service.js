d45a71a453e11da631794fa0fab1d8a13134c515
snychka <stefannychka@gmail.com>
2020-04-21 13:10:48 -0600

    cannot pass task 2 mod 5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 function getDirectoryContents(files, currentDir, query) {
 }
 
 function isDirectory(currentDir, file) {
+  let p = path.join(currentDir, file);
+  const fileInfo = fs.statSync(p);
+  //const fileInfo = fs.statSync(path.join(currentDir, file));
+  return fileInfo.isDirectory();
 }
 
 function readDir(currentDir, res, query) {
