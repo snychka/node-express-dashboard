a58abb9b2fa96bd1f7258bb11ecb656736575a51
snychka <stefannychka@gmail.com>
2020-04-28 10:57:35 -0600

    mod 6 task 4 error

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const settingsFilePath = path.join(__dirname, '../json/settings.json');
+//const settingsFilePath = path.join(__dirname + '../json/settings.json');
+
 function getSettings() {
-  return {}
+  const settingsData = fs.readFileSync(settingsFilePath);
+  return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2);
+  try {
+    fs.writeFileSync( settingsFilePath, settingsJSON );
+    return true;
+  } catch() {
+  //} catch {
+    return false;
+  }
 }
 
 function getDefaultDir() {
